40e3dba86876032988a7c85f18e40325afc08719
ChrisMarSilva <chris.mar.silva@gmail.com>
2022-10-05 17:37:48 +0100

    add tests upload file on aws s3

--- cms.golang.teste.arquivos/cms.golang.teste.ler.dir.and.upload/go.mod
+++ cms.golang.teste.arquivos/cms.golang.teste.ler.dir.and.upload/go.mod
@@ -1,3 +1,8 @@
 module github.com/chrismarsilva/cms.golang.teste.ler.dir.and.upload
 
 go 1.19
+
+require (
+	github.com/aws/aws-sdk-go v1.44.111 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
--- cms.golang.teste.arquivos/cms.golang.teste.ler.dir.and.upload/main.go
+++ cms.golang.teste.arquivos/cms.golang.teste.ler.dir.and.upload/main.go
@@ -5,24 +5,58 @@ import (
 	"log"
 	"os"
 	"time"
+	"fmt"
+	"sync"
+	"bytes"
+
+	"github.com/aws/aws-sdk-go/aws"
+	//"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	// "github.com/aws/aws-sdk-go/aws/awserr"
+	// "github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 // go mod init github.com/chrismarsilva/cms.golang.teste.ler.dir.and.upload
-// go get -u xxxxxxxxx
+// go get -u github.com/aws/aws-sdk-go
 // go mod tidy
 
 // go run main.go
+
+var (
+	client *s3.S3
+	wg     sync.WaitGroup
+)
+
+func init() {
+	sess := session.Must(session.NewSession())
+	creds := stscreds.NewCredentials(sess, "myRoleArn")
+	client = s3.New(sess, &aws.Config{Credentials: creds})
+
+	// creds := credentials.NewCredentials(os.Getenv("AWS_ACCESS_KEY"), os.Getenv("AWS_SECRET_KEY"), "")
+	// region := aws.String("us-east-1") // aws.StringValue(sess.Config.Region)
+
+	// sess, err := session.NewSession(&aws.Config{Credentials: creds, Region: region})
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// client = s3.New(sess)
+}
 
 func main() {
 	log.Println("INI")
 	var start time.Time = time.Now()
 
-	dir, err := os.Open("C:\\Users\\chris\\AppData\\Local\\Temp")
+	path := "C:\\Users\\chris\\AppData\\Local\\Temp"
+	dir, err := os.Open(path)
 	if err != nil {
-		log.Println("Error os.Open(): " + err.Error())
+		log.Println("Error os.Open(Path): " + err.Error())
 		return
 	}
 
+	semaforo := make(chan struct{}, 3) // 1_000
 	for {
 		files, err := dir.Readdir(1) // ler 1 arquivo por vez
 		if err != nil {
@@ -32,8 +66,49 @@ func main() {
 			log.Println("Error os.Readdir(): " + err.Error())
 			continue
 		}
-		log.Println("Name: " + files[0].Name())
+		// log.Println("Name: " + files[0].Name())
+		wg.Add(1)
+		semaforo <- struct{}{}
+		go upload(files[0], semaforo)
 	}
 
+	wg.Wait()
+
 	log.Println("FIM:", time.Since(start))
+}
+
+func upload(fileInfo os.FileInfo, semaforo <-chan struct{}) {
+	defer wg.Done()
+
+	fmt.Printf("Upload started: %s\n", fileInfo.Name())
+
+	filepath := fmt.Sprintf("C:\\Users\\chris\\AppData\\Local\\Temp\\%s", fileInfo.Name())
+	file, err := os.Open(filepath)
+	if err != nil {
+		<-semaforo
+		// log.Println("Error os.Open(FilePath): " + err.Error())
+		fmt.Printf("ERRO file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	var fileSize int64 = fileInfo.Size()
+
+	fileBuffer := make([]byte, fileSize)
+	file.Read(fileBuffer)
+
+	_, err = client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String("2kk-aprenda-golang"),
+		Key:    aws.String(fileInfo.Name()),
+		Body:   bytes.NewReader(fileBuffer),
+	})
+	if err != nil {
+		<-semaforo
+		// log.Println("Error client.PutObject(): " + err.Error())
+		fmt.Printf("ERRO upload: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Upload finished: %s\n", fileInfo.Name())
+	<-semaforo
 }
